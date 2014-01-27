f4d07f9e17bd4de11c11ca555b1bd7ffe307248b
Zeuxis Lo <seekstudio@gmail.com>
2014-01-27 15:11:54 +0800

    updated description in token usage

--- drop.py
+++ drop.py
@@ -7,7 +7,7 @@ from chest import robot
 if __name__ == "__main__":
     parser = optparse.OptionParser(usage="Usage: %prog -t [TOKEN]")
 
-    parser.add_option("-t", "--token", action="store", dest="token", help="Enter token for request curator api")
+    parser.add_option("-t", "--token", action="store", dest="token", help="Token for request curator api")
 
     (options, args) = parser.parse_args()
 
